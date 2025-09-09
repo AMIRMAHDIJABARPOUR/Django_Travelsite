2d49a252f438aa1ec5e95bd455a2371aede80d2d
AMIRMAHDIJABARPOUR <95540461+AMIRMAHDIJABARPOUR@users.noreply.github.com>
2025-09-10 03:10:24 +0330

    preparation templates

--- blog_page/urls.py
+++ blog_page/urls.py
@@ -18,9 +18,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path ,include
 from Django_Travelsite import settings
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("",include("home_page.urls")),
-    path("blog/",include("blog_page.urls")),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+]
