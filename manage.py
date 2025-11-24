01c588d23a9c693d9edab391ad4da262e63c9e3c
AMIRMAHDIJABARPOUR <95540461+AMIRMAHDIJABARPOUR@users.noreply.github.com>
2025-11-24 03:32:25 +0330

    bug fixed

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_Travelsite.settings.prod")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_Travelsite.settings.dev")
 
     try:
         from django.core.management import execute_from_command_line
