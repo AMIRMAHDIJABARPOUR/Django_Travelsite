1c1ad7c7cc9f999fcc4e63c66662e7461c30a79a
AMIRMAHDIJABARPOUR <95540461+AMIRMAHDIJABARPOUR@users.noreply.github.com>
2026-07-08 01:23:47 +0330

    remove trashed files

--- staticfiles/js/login.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toasts = document.querySelectorAll('.xai-message-toast');
-    toasts.forEach(toast => {
-        setTimeout(() => {
-            toast.classList.add('fade-out');
-            setTimeout(() => toast.remove(), 300); // Matches fadeOut duration
-        }, 5000); // 5 seconds
-    });
-});
